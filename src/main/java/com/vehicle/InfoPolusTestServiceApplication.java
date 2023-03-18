e0308301be3dda64f5783cff48b23d1a03da338e
Denys Pozyvailov <dpozyvailov@cybervisiontech.com>
2023-03-18 22:02:16 +0200

    Initial commit

---
+++ src/main/java/com/vehicle/InfoPolusTestServiceApplication.java
@@ -0,0 +1,15 @@
+package com.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoPolusTestServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InfoPolusTestServiceApplication.class, args);
+
+
+  }
+
+}
